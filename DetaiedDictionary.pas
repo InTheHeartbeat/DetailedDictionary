3658a8a5826a578011e81770f2f68fb89ceac201
Eugene fonBlaser <fonBlaser@gmail.com>
2019-05-07 15:19:37 +0300

    Update arrays to dynamic

--- DetaiedDictionary.pas
+++ DetaiedDictionary.pas
@@ -1,21 +1,52 @@
 program DetailedDictionary;
 
-var words            : array[0..100] of string;
-    wordsLength      : integer;
+var words            : array of string;
+    wordBases        : array of string;
+    wordTypes        : array of string;
+    wordPrepos       : array of string;
+    endings          : array of string;
+    vocals           : array of char;
+   
+procedure PrintArray(arr: array of char);
+var i : integer;
+begin
+    for i := 0 to arr.Length - 1 do
+      Writeln(arr[i]);
+end;
 
-    endings          : array[0..2]   of string;
-    endingsLength    : integer;
-    
+procedure AddCharToArray(ch: char);
+begin
+    SetLength(vocals, vocals.Length + 1);
+    vocals[vocals.Length - 1] := ch;
+end;
+   
+procedure InitializeVocals();
+begin
+    SetLength(vocals, 0);
+    AddCharToArray('à');
+    AddCharToArray('ó');
+    AddCharToArray('û');
+    AddCharToArray('å');
+end;
+   
 procedure InitializeEndings();
 begin
-  endings[0] := 'ed';
-  endings[1] := 'es';
-  endings[2] := 's';
+    SetLength(endings, 0);
+end;
+
+procedure InitializeStorage();
+begin
+    SetLength(words, 0);
+    SetLength(wordBases, 0);
+    SetLength(wordTypes, 0);
+    SetLength(wordPrepos, 0);
 end;
 
 procedure Initialize();
 begin
+  InitializeVocals();
   InitializeEndings();
+  InitializeStorage();
 end;
 
 begin
